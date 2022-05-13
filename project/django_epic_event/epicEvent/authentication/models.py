a247e04e31d31b432b234b73c9a7c110022edb9d
zionhigt <larcher501@gmail.com>
2022-05-13 18:18:16 +0200

    add managers constraint

--- project/django_epic_event/epicEvent/authentication/models.py
+++ project/django_epic_event/epicEvent/authentication/models.py
@@ -1,6 +1,19 @@
 from django.contrib.auth.models import AbstractUser, Permission
 from django.db import models
+from django.contrib.auth.models import Group
 
+
+class Role(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+    @classmethod
+    def get_manager_role_id(self):
+        return self.objects.get_or_create(name="MANAGER")[0].id
+
+        
 class User(AbstractUser):
     first_name = models.CharField(max_length=25)
     last_name = models.CharField(max_length=25)
@@ -9,13 +22,26 @@ class User(AbstractUser):
     manager_id = models.ForeignKey("authentication.User", verbose_name="manager", on_delete=models.SET_NULL, related_name="manager", blank=True, null=True)
     role = models.ForeignKey("authentication.Role", on_delete=models.SET_NULL, blank=True, null=True)
 
+
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=["email"], name="unique_user")
+            models.UniqueConstraint(fields=["email"], name="unique_user"),
+            models.CheckConstraint(
+                name="is_able_to_be_manager",
+                check=models.Q(manager_id=Role.get_manager_role_id())
+            ),
         ]
 
-class Role(models.Model):
-    name = models.CharField(max_length=25)
+    
+    def set_role(self, role_id):
+        """Ensure that role corespond whith permission groups
 
-    # def __str__(self):
-    #     return self.name
+        Args:
+            role_id (int): private key of a role record
+        """
+        role = Role.objects.get(pk=int(role_id))
+        group = Group.objects.get(name=role.name.lower() + 's')
+        # Remove all groups for user, ensure one role = one group
+        [self.groups.remove(grp) for grp in list(self.groups.all())]
+        self.groups.add(group)
+
