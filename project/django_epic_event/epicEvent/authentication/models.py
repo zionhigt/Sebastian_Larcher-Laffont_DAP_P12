f6c7d973247872e0a818aea23a2037c8479f823a
zionhigt <larcher501@gmail.com>
2022-05-14 11:05:56 +0200

    manger relation constraint

--- project/django_epic_event/epicEvent/authentication/models.py
+++ project/django_epic_event/epicEvent/authentication/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser, Permission
 from django.db import models
 from django.contrib.auth.models import Group
+from django.forms import ValidationError
 
 
 class Role(models.Model):
@@ -13,13 +14,20 @@ class Role(models.Model):
     def get_manager_role_id(self):
         return self.objects.get_or_create(name="MANAGER")[0].id
 
+def manager_validator(manager_id):
+    try:
+        manager = User.objects.get(pk=manager_id)
+    except User.DoesNotExist:
+        raise ValidationError("User not found for relation manager_id")
+    if manager_id != Role.get_manager_role_id():
+        raise ValidationError("This user cannot be assigned as a manager")
         
 class User(AbstractUser):
     first_name = models.CharField(max_length=25)
     last_name = models.CharField(max_length=25)
     email = models.CharField(max_length=100)
     password = models.CharField(max_length=128)
-    manager_id = models.ForeignKey("authentication.User", verbose_name="manager", on_delete=models.SET_NULL, related_name="manager", blank=True, null=True)
+    manager_id = models.ForeignKey("authentication.User", verbose_name="manager", on_delete=models.SET_NULL, related_name="manager", blank=True, null=True, validators=[manager_validator])
     role = models.ForeignKey("authentication.Role", on_delete=models.SET_NULL, blank=True, null=True)
 
 
@@ -39,8 +47,14 @@ class User(AbstractUser):
         Args:
             role_id (int): private key of a role record
         """
-        role = Role.objects.get(pk=int(role_id))
-        group = Group.objects.get(name=role.name.lower() + 's')
+        try:
+            role = Role.objects.get(pk=int(role_id))
+        except Role.DoesNotExist:
+            raise ValidationError("Entity not found", code="invalide")
+        try:
+            group = Group.objects.get(name=role.name.lower() + 's')
+        except Group.DoesNotExist:
+            raise ValidationError("Entity not found", code="invalide")
         # Remove all groups for user, ensure one role = one group
         [self.groups.remove(grp) for grp in list(self.groups.all())]
         self.groups.add(group)
