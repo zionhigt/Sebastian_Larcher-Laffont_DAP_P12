82b3301a3af76ffeb06a2c6ca16b2a7bb006bd6b
zionhigt <larcher501@gmail.com>
2022-05-15 14:38:32 +0200

    INIT views

--- project/django_epic_event/epicEvent/event/views.py
+++ project/django_epic_event/epicEvent/event/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from event.serializers import EventListSerializer, EventUpdateSerializer, EventDetailSerializer
+
+
+class EventViewSet(ModelViewSet):
+    list_serializer_class = EventListSerializer
+    detail_serializer_class = EventDetailSerializer
+    update_serializer_class = EventUpdateSerializer
+    permission_classes = []
+
+    def get_queryset(self):
+        pass
+
+    def get_serializer_class(self):
+        if self.action in ['update', 'partial_update']:
+            return self.update_serializer_class
+        if self.action in ['list']:
+            return self.list_serializer_class
+        if self.action in ['retrieve', 'create']:
+            return self.detail_serializer_class
+
+        return super().get_serializer_class()
+
