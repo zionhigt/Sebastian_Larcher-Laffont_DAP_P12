d5263e9986ee49e14479d1edd285eb99dcd730c2
zionhigt <larcher501@gmail.com>
2022-05-15 19:54:13 +0200

    Attach perms to viewsets

--- project/django_epic_event/epicEvent/event/views.py
+++ project/django_epic_event/epicEvent/event/views.py
@@ -2,13 +2,14 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 
 from event.serializers import EventListSerializer, EventUpdateSerializer, EventDetailSerializer
+from event.permissions import IsAuthorOrReadOnly
 
 
 class EventViewSet(ModelViewSet):
     list_serializer_class = EventListSerializer
     detail_serializer_class = EventDetailSerializer
     update_serializer_class = EventUpdateSerializer
-    permission_classes = [IsAuthenticated, ]
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly, ]
 
     def get_queryset(self):
         pass
