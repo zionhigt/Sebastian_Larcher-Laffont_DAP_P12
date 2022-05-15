9a5933703e7e46dec59b73887871887591cf2e7f
zionhigt <larcher501@gmail.com>
2022-05-15 13:58:32 +0200

    INIT serializers

--- project/django_epic_event/epicEvent/authentication/views.py
+++ project/django_epic_event/epicEvent/authentication/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.exceptions import PermissionDenied
+from .serializers import RegisterSerializer
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+def is_manager(user):
+    return user.__str__() != "AnonymousUser" and user.role.name == "MANAGER"
+
+
+class RegisterViewSet(ModelViewSet):
+
+    @classmethod
+    def as_view(self, *args, **kwargs):
+        return super().as_view({'post': "create"})
+
+    serializer_class = RegisterSerializer
+
+    def create(self, request):
+        if is_manager(request.user):
+            return super().create(request)
+        else:
+            raise PermissionDenied()
