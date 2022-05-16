82cf85d03234a37bdeb76eb909b333cb654bb03b
zionhigt <larcher501@gmail.com>
2022-05-16 20:18:43 +0200

    Miss that

--- project/django_epic_event/epicEvent/contrat/views.py
+++ project/django_epic_event/epicEvent/contrat/views.py
@@ -1,7 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 
-from contrat.serializers import ContratListSerializer, ContratUpdateSerializer, ContratDetailSerializer
+from contrat.serializers import (
+    ContratListSerializer,
+    ContratUpdateSerializer,
+    ContratDetailSerializer,
+    ContratSignSerializer
+)
 from contrat.permissions import IsAuthorOrReadOnly
 
 
@@ -9,6 +14,7 @@ class ContratViewSet(ModelViewSet):
     list_serializer_class = ContratListSerializer
     detail_serializer_class = ContratDetailSerializer
     update_serializer_class = ContratUpdateSerializer
+    sign_serializer_class = ContratSignSerializer
     permission_classes = [IsAuthenticated, IsAuthorOrReadOnly, ]
 
     def get_queryset(self):
@@ -21,6 +27,8 @@ class ContratViewSet(ModelViewSet):
             return self.list_serializer_class
         if self.action in ['retrieve', 'create']:
             return self.detail_serializer_class
+        if self.action in ['sign']:
+            return self.sign_serializer_class
 
         return super().get_serializer_class()
 
