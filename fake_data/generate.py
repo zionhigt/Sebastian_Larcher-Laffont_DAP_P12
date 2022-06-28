7afcd27d7b2b0cd3eb43b457685343104075a300
zionhigt <larcher501@gmail.com>
2022-06-28 19:53:54 +0200

    Be PEP8 Friendly

--- fake_data/generate.py
+++ fake_data/generate.py
@@ -2,6 +2,7 @@ from faker import Faker
 from random import randint
 import json
 
+
 fake = Faker()
 
 ALL = {
@@ -46,6 +47,7 @@ FIELD_MAPPING = {
     },
 }
 
+
 def manager_set():
     manager = get_user(role=1, manager_id=None)
     manager_id = manager.get("id")
@@ -66,35 +68,42 @@ def manager_set():
                 ALL.get("event", []).append(event)
     print("MANAGER SET GENERATED")
 
+
 def get_salesman_set(manager_id):
     salesman = get_user(role=3, manager_id=manager_id)
     salesman_id = salesman.get("id")
     return salesman_id, salesman
+
 
 def get_support_set(manager_id):
     support = get_user(role=2, manager_id=manager_id)
     support_id = support.get("id")
     return support_id, support
 
+
 def get_customer_set(salesman_id):
     customer = get_customer(sale_contact_id=salesman_id)
     customer_id = customer.get("id")
     return customer_id, customer
+
 
 def get_contrat_set(customer_id, salesman_id):
     contrat = get_contrat(customer_id, salesman_id)
     contrat_id = contrat.get("id")
     return contrat_id, contrat
 
+
 def get_event_set(support_id, contrat_id):
     event = get_event(support_contact_id=support_id, contrat_id=contrat_id)
     event_id = event.get("id")
     return event_id, event
 
+
 def get_id(table):
     _id = CURRENT_IDS.get(table)
     CURRENT_IDS.update({table: _id + 1})
     return _id
+
 
 def get_user(role, manager_id):
     id = get_id("user")
@@ -115,6 +124,8 @@ def get_user(role, manager_id):
         "date_joined": fake.date(),
         "is_superuser": False,
     }
+
+
 def get_contrat(customer_id, salesman_id):
     id = get_id("contrat")
     return {
@@ -127,6 +138,8 @@ def get_contrat(customer_id, salesman_id):
         "amount": randint(1000, 6000),
         "payment_due": fake.date(),
     }
+
+
 def get_customer(sale_contact_id):
     id = get_id("customer")
     return {
@@ -143,6 +156,8 @@ def get_customer(sale_contact_id):
         "phone": fake.phone_number()[:20],
 
     }
+
+
 def get_event(support_contact_id, contrat_id):
     id = get_id("event")
     name = "%s %s" % (fake.first_name(),  fake.color_name())
@@ -159,12 +174,15 @@ def get_event(support_contact_id, contrat_id):
 
     }
 
+
 def get_data_set():
     for rnd_manager in range(0, randint(1, 5)):
         manager_set()
 
+
 def extract_keys(dictionary):
     return dictionary.keys()
+
 
 def remap(table, map):
     new_map = []
@@ -175,6 +193,7 @@ def remap(table, map):
             new_record.update({new_key: value})
         new_map.append(new_record)
     return new_map
+
 
 def prepare_insert_query(table):
     psql_table_name = PSQL_TABLE_NAME.get(table)
@@ -192,15 +211,15 @@ def prepare_insert_query(table):
 
     return insert_query
 
+
 def prepare_query():
     get_data_set()
     for TABLE in ALL.keys():
         yield TABLE, prepare_insert_query(TABLE)
+
 
 records = prepare_query()
 for table, record in records:
     with open("./data/%s.sql" % table, "w") as data_file:
         data_file.writelines(record)
         data_file.close()
-
-
